e867b8aa2f01aa0549205dadd278929775ad9f0b
mxwiser <neoway@126.com>
2023-02-10 20:36:28 +0800

    init

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <Inc/hello.h>
-int main(){
+#include <mysql.h>
 
-    printf("hello\n");
+
+
+
+int main(){
+    MYSQL *conn;
+
+     conn=mysql_init(NULL);
+
+    if (conn == NULL) { // 如果返回NULl说明初始化失败
+
+    printf("mysql_init failed!\n");
+    }
+
+    int k;
+    scanf("%d",&k);
+    printf("hello %d\n",k);
     return 0;
 }
